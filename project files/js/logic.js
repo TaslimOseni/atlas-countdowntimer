74feb05b9881a37f01c4fbc0170e8da16af323aa
TaslimOseni <tasleemoseni@gmail.com>
2018-10-08 14:20:10 +0100

    Replaced all document.getElement() with variables

--- project files/js/logic.js
+++ project files/js/logic.js
@@ -3,9 +3,14 @@ function press_start(duration){
 
 		duration -= 1;	//I decremented by 1 to cater for the lag that occurs when the timer starts
 
+		//initialization of variables
+		var timer_text = document.getElementById('timer_text');
+		var start_button = document.getElementById('start_button');
+		var reset_button = document.getElementById('reset_button');
 
-		document.getElementById('timer_text').style.color = "#00FF00";	//turn the text colour to green
-		document.getElementById('start_button').innerHTML = "STOP";		//change button text from 'start' to 'stop'
+
+		timer_text.style.color = "#00FF00";	//turn the text colour to green
+		start_button.innerHTML = "STOP";		//change button text from 'start' to 'stop'
 
 
 		//I gave the value of duration to variables timer, minutes and seconds.
@@ -23,50 +28,50 @@ function press_start(duration){
 	        seconds = seconds < 10 ? "0" + seconds : seconds;
 
 	        //Setting the final time.. Since we'd always be counting down from an hour, I left the hour field as '00'
-	        document.getElementById('timer_text').innerHTML = "00:" + minutes + ":" + seconds;
+	        timer_text.innerHTML = "00:" + minutes + ":" + seconds;
 
 	        //Here, we're checking whether the timer has elapsed (00:00:00)
 	        if(--the_timer < 0){
 	            clearInterval(my_countdowntimer);
-	            document.getElementById('timer_text').style.color = "#0000FF";
-				document.getElementById('timer_text').innerHTML = "TIME ELAPSED!!!";
-				document.getElementById('reset_button').onclick = function(){
-					document.getElementById('timer_text').style.color = "#FF0000";
-					document.getElementById('timer_text').innerHTML = "01:00:00";
-					document.getElementById('start_button').innerHTML = "START";
-					document.getElementById('start_button').onclick = function(){
+	            timer_text.style.color = "#0000FF";
+				timer_text.innerHTML = "TIME ELAPSED!!!";
+				reset_button.onclick = function(){
+					timer_text.style.color = "#FF0000";
+					timer_text.innerHTML = "01:00:00";
+					start_button.innerHTML = "START";
+					start_button.onclick = function(){
 					press_start(3599);
 						}
 					}
-				document.getElementById('start_button').innerHTML = "-----";
-				document.getElementById('start_button').onclick = null;
+				start_button.innerHTML = "-----";
+				start_button.onclick = null;
 
 	        }
 	    }, 1000);
 
 
 	    //when reset is pressed during countdown
-	    document.getElementById('reset_button').onclick = function(){
+	    reset_button.onclick = function(){
 	    		clearInterval(my_countdowntimer);
-	    		document.getElementById('timer_text').style.color = "#FF0000";
-				document.getElementById('timer_text').innerHTML = "01:00:00";
-				document.getElementById('start_button').innerHTML = "START";
-				document.getElementById('start_button').onclick = function(){
+	    		timer_text.style.color = "#FF0000";
+				timer_text.innerHTML = "01:00:00";
+				start_button.innerHTML = "START";
+				start_button.onclick = function(){
 					press_start(3599);
 				}
 	    }
 
 	    //when start button is pressed during countdown
-	    document.getElementById('start_button').onclick = function(){
+	    start_button.onclick = function(){
 	    	//if the text on the start-button is 'stop'
-	    	if(document.getElementById('start_button').textContent === 'STOP'){
+	    	if(start_button.textContent === 'STOP'){
 	    		clearInterval(my_countdowntimer);
-	    		document.getElementById('start_button').innerHTML = "START";
-	    		document.getElementById('timer_text').style.color = "#FFFF00";
+	    		start_button.innerHTML = "START";
+	    		timer_text.style.color = "#FFFF00";
 	    	}
 
 	    	//if the text on the start-button is 'start'
-	    	else if(document.getElementById('start_button').textContent === 'START'){
+	    	else if(start_button.textContent === 'START'){
 	    		press_start(getRemainderDuration());
 	    	}
 	    }
@@ -76,7 +81,7 @@ function press_start(duration){
 
 function getRemainderDuration(){
 	//get the string version of the time left
-	var timeLeft = document.getElementById('timer_text').textContent;
+	var timeLeft = timer_text.textContent;
 
 	//extract the seconds and minutes from the string version above and return it
 	return (parseInt(timeLeft.charAt(3) + timeLeft.charAt(4)) * 60) + parseInt(timeLeft.charAt(6) + timeLeft.charAt(7));
